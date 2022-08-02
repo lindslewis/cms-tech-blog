9c87896c6d26a1a7578a1cca92efe40afcd0a48f
LindsayLew <lmschwehr@gmail.com>
2022-08-01 21:54:04 -0700

    user model

---
+++ models/User.js
@@ -0,0 +1,61 @@
+const {Model, DataTypes} = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPass) {
+        return bcrypt.compareSync(loginPass, this.password);
+    }
+};
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8],
+            },
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserInfo) =>{
+                // read that 10 salt rounds recommended for password hashing
+                newUserInfo.password = await bcrypt.hash(newUserInfo.password, 10);
+                return newUserInfo;
+            },
+            beforeUpdate: async (updateUserInfo) =>{
+                updateUserInfo.password = await bcrypt.hash(updateUserInfo.password, 10);
+                return updateUserInfo;
+            },
+        },
+    // auto-pluralization is stopped with freeze table name. underscored turns names into snake-cased versions
+    
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;

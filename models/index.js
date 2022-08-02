998c4a04455ff814957e6d8e1aed694ab09f4cd9
LindsayLew <lmschwehr@gmail.com>
2022-08-01 22:01:01 -0700

    note to self

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
 const User = require('./User');
 const blogPost = require('./blogPost');
 // this is where the has many and whatnot lives
+
+// should probably make those little image table things for the routes

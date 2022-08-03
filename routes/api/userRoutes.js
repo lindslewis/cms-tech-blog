19cd9a61c71715fae370df1d3294af8d441cca4a
LindsayLew <lmschwehr@gmail.com>
2022-08-02 17:45:07 -0700

    get route for user info

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 const { Comment, blogPost, User } = require('../../models');
+const express = require('express');
+const bcrypt = require('bcrypt');
 
-
+// routes needed:
+// get users. user creation and password (post route). 
 // endpoint is `/api/users`
 
-router.get('/', async (req,res))
+router.get('/', async (req,res)=>{
+    User.findAll({
+        include: [Comment, blogPost]
+    }).then(data=>{
+        res.json(data)
+    }).catch(err=>{
+        res.status(500).json({msg:"ERROR",err})
+    })
+});

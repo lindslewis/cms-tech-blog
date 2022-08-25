b39e6e386f3d61b4e893fe7e89d867459b60a3c5
LindsayLew <lmschwehr@gmail.com>
2022-08-24 20:37:57 -0700

    comment delete

--- routes/api/commentRoutes.js
+++ routes/api/commentRoutes.js
@@ -36,3 +36,27 @@ router.post('/:blogPost_id/comments', withAuth, async (req, res) => {
         res.status(500).json({ message: "Error", err})
     })
 });
+
+// delete a comment
+router.delete('/:blogPost_id/comments/:id', withAuth, async (req, res) => {
+    try {
+        const commentInfo = await Comment.destroy({
+            where: {
+                // do I need to do the blogpost id here too?
+                id:req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!commentInfo) {
+            res.status(404).json({ message: "no comment found" });
+            return;
+        }
+
+        res.status(200).json(commentInfo);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;

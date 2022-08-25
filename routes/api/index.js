2a2b9f928bc38d3d4eb9d40c759c6358b24fbb2a
LindsayLew <lmschwehr@gmail.com>
2022-08-24 18:31:31 -0700

    user login

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
-const blogRoutes = require('./postRoutes');
+// const blogRoutes = require('./postRoutes');
 
 router.use('/users', userRoutes);
 router.use('/comments', commentRoutes);
-router.use('/posts', blogRoutes);
+// router.use('/posts', blogRoutes);
 
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -16,3 +16,47 @@ router.get('/', async (req,res)=>{
         res.status(500).json({msg:"ERROR",err})
     })
 });
+
+// create user
+router.post('/', async (req, res) => {
+    try {
+        const userInfo = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userInfo.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userInfo);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+// login existing user
+router.post('/login', async (req, res) => {
+    try {
+        const userInfo = await User.findOne({ where: { email: req.body.email } });
+
+        if(!userInfo) {
+            res.status(400).json({ message: "Incorrect login credentials."});
+            return;
+        }
+        const validPassword = await userInfo.checkPassword(req.body.password);
+
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect login credentials.' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userInfo.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userInfo, message: "You have been logged in."})
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+

431410a7e94192f2fe2b0653b7be3a1d8d13cedc
LindsayLew <lmschwehr@gmail.com>
2022-08-30 16:06:22 -0700

    home routes complete

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -73,3 +73,14 @@ router.get('/dashboard', withAuth, async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
+});
+
+module.exports = router;

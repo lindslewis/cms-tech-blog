24851f8c5030eefe958b9dfc1c82c993a4d14137
LindsayLew <lmschwehr@gmail.com>
2022-08-30 16:26:13 -0700

    set up session

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
-const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: "secret secrety secrets",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

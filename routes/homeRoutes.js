030a2d02c5c55ef178ed88a06c4cef522a605238
LindsayLew <lmschwehr@gmail.com>
2022-08-30 09:16:31 -0700

    render home data

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,3 +1,31 @@
 const router = require('express').Router();
 const { Comment, User, blogPost } = require('../models');
 const withAuth = require('../utils/auth');
+
+// get home page
+router.get('/', async (req, res) => {
+    try {
+        // need to grab all the blogposts and join it with the user data?? 
+        // I feel like I need to include the comments as well -- no not in this one
+        const blogData = await blogPost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                // {
+                //     model: Comment,
+                //     attributes: ['comment_content'],
+                // },
+            ],
+        });
+
+        const blogs = blogData.map((blog) =>
+        blog.get({ plain: true }));
+
+        res.render('home', {
+            blogs,
+            logged_in: req.session.logged_in
+        });
+    } catch (err)
+})

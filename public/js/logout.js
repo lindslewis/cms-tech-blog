8aea302f52599bcca9c92448c9b97689a3ce3284
LindsayLew <lmschwehr@gmail.com>
2022-08-30 20:09:17 -0700

    logout js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,3 +1,13 @@
 
 
 // need to logout, path is already made so literally just the function
+
+const logout = async () => {
+    // this is the back end path
+    const res = await fetch('/api/users/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    
+}

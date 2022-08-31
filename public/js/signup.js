bd159bed6acb8d4f6648cf481958c732eb8fd508
LindsayLew <lmschwehr@gmail.com>
2022-08-30 20:43:48 -0700

    finished sign up

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,4 +4,20 @@ const signUpFormHandler = async (e) => {
     const username = document.querySelector('#inputUser').value.trim();
     const email = document.querySelector('#inputEmail').value.trim();
     const password = document.querySelector('#inputPassword').value.trim();
-}
+
+    if (username && email && password) {
+        const res = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (res.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(res.statusText);
+        }
+    }
+};
+
+document.querySelector('.signUpForm').addEventListener('submit', signUpFormHandler);
